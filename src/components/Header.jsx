fbda54c9e6e5a28d7c943f17cd27ba3e8fa9b352
Safdar-Ali-India <safdarali63166@gmail.com>
2024-11-27 00:08:34 +0530

    Created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => (
+  <header className="header">
+    <h1>Tech.Care</h1>
+    <div className="doctor-profile">
+      <p>Dr. Jose Simmons</p>
+    </div>
+  </header>
+);
+
+export default Header;
